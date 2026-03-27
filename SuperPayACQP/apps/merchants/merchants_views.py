e5aeadc94a8b1fdc4a1cf354009b0301e31df3ae
Kong Hao Yang <konghaoyang@U-JLC79D04-2323.local>
2026-03-27 13:40:07 +0800

    fix true whitespace issue

--- SuperPayACQP/apps/merchants/merchants_views.py
+++ SuperPayACQP/apps/merchants/merchants_views.py
@@ -339,8 +339,8 @@ class EntryCodeConfirmView(APIView):
             ),
                         paymentMethod=request_dto.paymentMethod,
                         paymentFactor=AlipayPaymentFactorDTO(
-                isInStorePayment="true " if request_dto.paymentFactor.isInStorePayment else "false",
-                isCashierPayment="true " if request_dto.paymentFactor.isCashierPayment else "false",
+                isInStorePayment="true" if request_dto.paymentFactor.isInStorePayment else "false",
+                isCashierPayment="true" if request_dto.paymentFactor.isCashierPayment else "false",
                 inStorePaymentScenario=request_dto.paymentFactor.inStorePaymentScenario
             ),
                         paymentExpiryTime=expiry_time.strftime("%Y-%m-%dT%H:%M:%S+08:00"),
